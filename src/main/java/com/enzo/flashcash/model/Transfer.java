3751b8dec14acc1afeef99078f0417420a030139
enzo-gavini <enzo.gavini@laplateforme.io>
2025-12-08 22:29:30 +0100

    Feat Transfer Link

---
+++ src/main/java/com/enzo/flashcash/model/Transfer.java
@@ -0,0 +1,22 @@
+package com.enzo.flashcash.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDateTime date;
+    @ManyToOne
+    private  User from;
+    @ManyToOne
+    private  User to;
+    private Double amountBeforeFee;
+    private Double amountAfterFee;
+}
